618a2a7b6437732d89d6c20e8baceb6d73088e89
shock59 <42130427+shock59@users.noreply.github.com>
2025-11-16 18:49:58 +1100

    Add marker to the guess map when clicking

--- src/maps/GuessMap.svelte
+++ src/maps/GuessMap.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-  import maplibregl from "maplibre-gl";
+  import maplibregl, { Marker } from "maplibre-gl";
   import { onMount } from "svelte";
   import esriStyle from "../esriStyle";
 
   let mapContainer: HTMLElement;
 
   onMount(() => {
-    new maplibregl.Map({
+    const map = new maplibregl.Map({
       container: mapContainer,
       style: esriStyle,
       center: [0, 0],
@@ -17,6 +17,24 @@
         customAttribution:
           '<a href="https://maplibre.org/">MapLibre</a> | Map tiles &copy; <a href="https://www.esri.com/">Esri</a> | Images from Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
       },
+    });
+
+    map.getCanvas().style.cursor = "pointer";
+    map.on("mouseup", () => {
+      map.getCanvas().style.cursor = "pointer";
+    });
+    map.on("mousedown", () => {
+      map.getCanvas().style.cursor = "";
+    });
+
+    let guessMarker: Marker | undefined = undefined;
+    map.on("click", (event) => {
+      if (!guessMarker) {
+        guessMarker = new Marker().setLngLat(event.lngLat).addTo(map);
+        guessMarker.getElement().style.cursor = "pointer";
+      } else {
+        guessMarker.setLngLat(event.lngLat);
+      }
     });
   });
 </script>
