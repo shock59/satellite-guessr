a569ebdb8236b19ba657df3b08ca1a749f8e6aa7
shock59 <42130427+shock59@users.noreply.github.com>
2025-11-16 17:37:30 +1100

    Add map

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,31 @@
 <script lang="ts">
+  import maplibregl from "maplibre-gl";
+  import { onMount } from "svelte";
+
+  let mapContainer: HTMLElement;
+
+  onMount(() => {
+    const map = new maplibregl.Map({
+      container: mapContainer,
+      style: "https://demotiles.maplibre.org/globe.json",
+      center: [0, 0],
+      zoom: 1,
+    });
+  });
 </script>
 
-<main>SatelliteGuessr</main>
+<main>
+  <div bind:this={mapContainer} id="map-container"></div>
+</main>
+
+<style>
+  main {
+    width: 100vw;
+    height: 100vh;
+  }
+
+  #map-container {
+    width: 100%;
+    height: 100%;
+  }
+</style>
